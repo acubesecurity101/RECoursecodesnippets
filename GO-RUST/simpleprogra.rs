b0b7b8282a5a993170e4093ad4245b629f56e87c
acubesecurity101 <87819412+acubesecurity101@users.noreply.github.com>
2021-07-26 00:29:11 +0530

    Add files via upload

---
+++ GO-RUST/simpleprogra.rs
@@ -0,0 +1,13 @@
+fn add(){
+
+    let a:i32 = 67;
+    let b:i32 = 33;
+    let c:i32 = a + b;
+    println!("The value of a + b is : {}", c);
+}
+
+fn main(){
+
+    println!("Jump to Void");
+    add();
+}
